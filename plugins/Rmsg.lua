40bc551f6a59f670cbd0da45c0e96d872c0bbb6e
MaTaDoRrOo <darvishim137196@gmail.com>
2017-03-15 18:45:10 +0330

    by MahDiRoO

--- plugins/Rmsg.lua
+++ plugins/Rmsg.lua
@@ -1,32 +1,37 @@
---Begin Rmsg.lua By @MahDiRoO
-local function delmsg (arg,data)
-    for k,v in pairs(data.messages_) do
+local function delmsg (i,naji)
+    msgs = i.msgs 
+    for k,v in pairs(naji.messages_) do
+        msgs = msgs - 1
         tdcli.deleteMessages(v.chat_id_,{[0] = v.id_}, dl_cb, cmd)
+        if msgs == 1 then
+            tdcli.deleteMessages(naji.messages_[0].chat_id_,{[0] = naji.messages_[0].id_}, dl_cb, cmd)
+            return false
+        end
     end
+    tdcli.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
 end
 local function run(msg, matches)
-    local chat_id = msg.chat_id_
-    local msg_id = msg.id_
-    if matches[1] == 'rmsg' or matches[1] == 'Rmsg' or matches[1] == 'حذف' then
-        if tostring(chat_id):match("^-100") then 
-            if is_owner(msg) then
-                if tonumber(matches[2]) > 100 or tonumber(matches[2]) < 1 then
-                    return  '🚫 *100*> _تعداد پیام های قابل حذف هر دفعه_ >*1* 🚫'
-                else
-                    tdcli.getChatHistory(chat_id, msg_id, 0, tonumber(matches[2]), delmsg, nil)
-                    return '*'..matches[2]..'* _پیام اخیر پاک شدند_'
-                end
+    if matches[1] == 'del' and is_owner(msg) or matches[1] == 'Del' and is_owner(msg) or matches[1] == 'rmsg' and is_owner(msg) or matches[1] == 'Rmsg' and is_owner(msg) or matches[1] == 'حذف' and is_owner(msg) then
+        if tostring(msg.to.id):match("^-100") then 
+            if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
+                return  '🚫 *1000*> _تعداد پیام های قابل پاک سازی در هر دفعه_ >*1* 🚫'
+            else
+				tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
+				return "`"..matches[2].." `_پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
             end
         else
-            return '_این قابلیت فقط در سوپرگروه ممکن است_'
+            return '⚠️ _این قابلیت فقط در سوپرگروه ممکن است_ ⚠️'
         end
     end
 end
-
-return {patterns = {
-        '^[!#/]([Rr]msg) (%d*)$',
-        '^([Rr]msg) (%d*)$',
-        '^(حذف) (%d*)$',
+return {
+    patterns = {
+        '^[!#/]([Dd]el) (%d*)$',
+		'^([Dd]el) (%d*)$',
+		'^[!#/]([Rr]msg) (%d*)$',
+		'^([Rr]msg) (%d*)$',
+		'^(حذف) (%d*)$',
     },
-    run = run}
---End Rmsg.lua--
+    run = run
+}
+--BY MahDiRoO
